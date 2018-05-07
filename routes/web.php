f382c194903e67e66f308b534e532ac49632d1ab
sheenazien8 <sheenazien08@gmail.com>
2018-05-07 09:55:51 +0700

    Update one to many user mempunyai banyak post

--- routes/web.php
+++ routes/web.php
@@ -145,16 +145,24 @@ Route::get('/read_post', function() {
 
 	$posts = $user->posts()->get();
 
-foreach ($posts as $post) {
-
-	$data[] =[
-		'name' => $post->user->name,
-		'post_id' => $post->id,
-		'title' => $post->title,
-		'body' => $post->body
+	foreach ($posts as $post) {
+		$data[] =[
+			'name' => $post->user->name,
+			'post_id' => $post->id,
+			'title' => $post->title,
+			'body' => $post->body
 	];
 
-	return $data;
-
 	}
+});
+
+Route::get('/update_post', function() {
+    $user = User::findOrFail(1);
+
+    $user->posts()->where('id', 2)->update([
+			'title' => 'post update id 2',
+			'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+		]);
+
+		return 'success';
 });
